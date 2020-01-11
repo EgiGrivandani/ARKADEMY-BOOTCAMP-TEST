53a073d973fd10798d0fc0265e0c66bb036743d6
EgiGrivandani <59734794+EgiGrivandani@users.noreply.github.com>
2020-01-11 21:05:22 +0700

    Soal_1

---
+++ 1.js
@@ -0,0 +1,17 @@
+var bio = {
+    "nama" : "EGi Grivandani",
+  "umur" : 21,
+  "address" : "Kp.Bayur Rt.06/03 Ds.Hegarmanah Kec.BabakanCikao Kab.Puwakarta",
+  "hobbies" : '{"Play Games", "Badminton", "ReadBook"}',
+  "is_married" : false,
+     "list_school" : '{"SDN 1 Hegarmanah", "Smpn 4 Purwakarta", "Smkn 1 Purwakarta", "Politeknik Perdana Mandiri"}',
+     "year_in" : '{"2004", "2010", "2013", "2016"}',
+     "year_out" : '{"2010", "2013", "2016", "2019"}',
+     "major"	: '{"null","null", "Teknik elektro", "Manajemen Informatika"}',
+     "skill" : '{"Html", "Css", "Php", "VisuaBasic"}',
+     "level" : '{"Advanced", "Advanced", "Beginner", "Advanced"}',
+     "interest_in_coding" : true
+     
+
+    }		
+var biodata = JSON.stringify(bio);  
